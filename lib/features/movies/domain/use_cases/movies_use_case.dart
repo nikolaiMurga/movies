b45c7454f92f5304570cd4e8576144622bfd75a5
nikolaiMurga <nicolai.murga@gmail.com>
2025-10-05 19:57:45 +0200

    - ref

--- lib/features/movies/domain/use_cases/movies_use_case.dart
+++ lib/features/movies/domain/use_cases/movies_use_case.dart
@@ -1,19 +1,15 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:movies/core/network/requests/currencies_request.dart';
-import 'package:movies/features/movies/data/repos/movie_local_repo.dart';
 import 'package:movies/features/movies/data/repos/movie_network_repo.dart';
 import 'package:movies/features/movies/domain/models/movie.dart';
 import 'package:movies/features/movies/domain/models/paginated_movies.dart';
 
-final moviesUseCase = Provider<MoviesUseCase>((ref) {
-  return MoviesUseCase(ref.watch(movieNetworkRepo));
-});
+final moviesUseCase = Provider<MoviesUseCase>((ref) => MoviesUseCase(ref.watch(movieNetworkRepo)));
 
 class MoviesUseCase {
   final MovieNetworkRepo _networkRepo;
-  // final MovieLocalRepo _localRepo;
 
-  MoviesUseCase(this._networkRepo,/* this._localRepo*/);
+  MoviesUseCase(this._networkRepo);
 
   Future<PaginatedMovies> fetchMovies({required MoviesRequest request}) async {
     return _networkRepo.fetchPaginatedMovies(request: request);
@@ -22,16 +18,4 @@ class MoviesUseCase {
   Future<List<Movie>> fetchMoviesBySearch({required String query}) async {
     return _networkRepo.fetchPaginatedMoviesBySearch(query: query);
   }
-
-  // Future<bool> saveFavoriteMovies({required List<Movie> list}) async {
-  //   return _localRepo.saveFavoriteMovies(list: list);
-  // }
-  //
-  // Future<List<Movie>> loadFavoriteMovies() async {
-  //   return _localRepo.loadFavoriteMovies();
-  // }
-  //
-  // Future<bool> removeFavoriteMovies() async {
-  //   return _localRepo.removeFavoriteMovies();
-  // }
 }
