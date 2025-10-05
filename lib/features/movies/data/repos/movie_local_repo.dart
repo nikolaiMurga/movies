96566c9edcea5ede8af942ccaba8b8b6169cc225
nikolaiMurga <nicolai.murga@gmail.com>
2025-10-05 11:42:26 +0200

    - ref renaming

--- lib/features/movies/data/repos/movie_local_repo.dart
+++ lib/features/movies/data/repos/movie_local_repo.dart
@@ -4,11 +4,11 @@ import 'package:movies/core/local_storage/local_storage_client.dart';
 import 'package:movies/core/mappers/movie_mapper.dart';
 import 'package:movies/features/movies/domain/models/movie.dart';
 
-class LocalRepo {
+class MovieLocalRepo {
   final LocalStorageClient _localStorageClient;
   final MovieMapper _movieMapper;
 
-  LocalRepo(this._localStorageClient, this._movieMapper);
+  MovieLocalRepo(this._localStorageClient, this._movieMapper);
 
   Future<bool> saveFavoriteMovies({required List<Movie> list}) async {
     final json = {'list': list.map((m) => _movieMapper.toJson(m)).toList()};
